9ed42619add9f6ca0c501cb2a166a36e05081219
DyDy2703 <duyhoang27031999@gmail.com>
2026-02-06 18:58:16 +0700

    Cập nhật word SRS chương 2.6 và khởi tạo project

---
+++ src/models/issue.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const issueSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  project: { type: mongoose.Schema.Types.ObjectId, ref: "Project" },
+  status: { type: String, enum: ["To Do", "In Progress", "Done"], default: "To Do"},
+  priority: { type: String, enum: ["Low", "Medium", "High"], default: "Medium" },
+  reporter: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  assignee: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  dueDate: Date
+}, { timestamps: true });
+
+module.exports = mongoose.model("Issue", issueSchema);
